4674f7555875b1f0255cb542dcb00a6169b028c8
Sven Bergmann <sven.bergmann@lhind.dlh.de>
2025-05-27 13:34:06 +0200

    Benutzer erweitert

--- Benutzer.py
+++ Benutzer.py
@@ -1,10 +1,17 @@
+BENUTZER_RECHTE = {
+    "Benutzer": ["Benutzer"],
+    "Lehrer": ["Benutzer", "Lehrer"],
+    "Admin": ["Benutzer", "Admin"],
+    "Schueler": ["Benutzer", "Schueler"]
+}
+
 class Benutzer:
-    def __init__(self, name, passwort=None, loginStatus=False):
+    def __init__(self, name, passwort=None, loginStatus=False, rolle="Benutzer"):
         self.name = name
         self.passwort = passwort
         self.loginStatus = loginStatus
-        self.rechte = []
-        self.rechte.append("Benutzer")
+        self.rolle = rolle
+        self.rechte = BENUTZER_RECHTE.get(rolle, [])
 
     def getBenutzername(self):
         return self.name
@@ -17,7 +24,17 @@ class Benutzer:
     
     def getRechte(self):
         return self.rechte
+    
 
-    
-# Kleinsten Rechte
-    
+class Lehrer(Benutzer):
+    def __init__(self, name, passwort=None, loginStatus=False):
+        super().__init__(name, passwort, loginStatus, rolle="Lehrer")
+
+class Admin(Benutzer):
+    def __init__(self, name, passwort=None, loginStatus=False):
+        super().__init__(name, passwort, loginStatus, rolle="Admin")
+
+class Schueler(Benutzer):
+    def __init__(self, name, passwort=None, loginStatus=False):
+        super().__init__(name, passwort, loginStatus, rolle="Schueler")
+
