5ed5325690d0a3459da30da978556f71783d2951
Nico Namsler <nico.namsler@lhind.dlh.de>
2025-05-27 13:44:20 +0200

    Hinzufügen von Getter- und Setter-Methoden &  Klassenmethode zur Anzeige der Benutzeranzahl.

--- Benutzer.py
+++ Benutzer.py
@@ -6,13 +6,17 @@ BENUTZER_RECHTE = {
 }
 
 class Benutzer:
+    anzahl_benutzer = 0  # Klassenattribut für die Gesamtanzahl
+
     def __init__(self, name, passwort=None, loginStatus=False, rolle="Benutzer"):
         self.name = name
         self.passwort = passwort
         self.loginStatus = loginStatus
         self.rolle = rolle
         self.rechte = BENUTZER_RECHTE.get(rolle, [])
+        Benutzer.anzahl_benutzer += 1
 
+    # Getter-Methoden
     def getBenutzername(self):
         return self.name
     
@@ -24,7 +28,25 @@ class Benutzer:
     
     def getRechte(self):
         return self.rechte
+
+    # Setter-Methoden
+    def setBenutzername(self, name):
+        self.name = name
     
+    def setPasswort(self, passwort):
+        self.passwort = passwort
+    
+    def setLoginStatus(self, status):
+        self.loginStatus = status
+    
+    def setRolle(self, rolle):
+        self.rolle = rolle
+        self.rechte = BENUTZER_RECHTE.get(rolle, [])
+
+    # Klassenmethode
+    @classmethod
+    def zeige_anzahl_benutzer(cls):
+        return cls.anzahl_benutzer
 
 class Lehrer(Benutzer):
     def __init__(self, name, passwort=None, loginStatus=False):
@@ -38,3 +60,15 @@ class Schueler(Benutzer):
     def __init__(self, name, passwort=None, loginStatus=False):
         super().__init__(name, passwort, loginStatus, rolle="Schueler")
 
+def getBenutzerKlasse(rolle):
+    if rolle == "Lehrer":
+        return Lehrer
+    elif rolle == "Admin":
+        return Admin
+    elif rolle == "Schueler":
+        return Schueler
+    else:
+        return Benutzer
+
+def getBenutzerRechte(rolle):
+    return BENUTZER_RECHTE.get(rolle, [])
